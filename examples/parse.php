88eea46da1c7a1d907625c29d2e5642dc7c16852
udger.com <info@udger.com>
2016-07-13 14:34:06 +0200

    small edit

--- examples/parse.php
+++ examples/parse.php
@@ -6,12 +6,9 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 $factory = new Udger\ParserFactory();
 $parser = $factory->getParser();
 
-// set data dir (this php script must right write to cache dir)
-$parser->setDataDir(sys_get_temp_dir() . "/udgercache/");
+// set data dir (directory containing the file udgerdb_v3.dat) 
+$parser->setDataDir(sys_get_temp_dir() . "/udgercache/");   
 
-// set You Acceskey (see https://udger.com/account/main) 
-//$parser->setAccessKey('XXXXXX'); 
-// or download the data manually from http://data.udger.com/[ACCESS_KEY]/udgerdb.dat
 
 $parser->setUA('Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.97 Safari/537.36');
 $parser->setIP("66.249.64.1");
